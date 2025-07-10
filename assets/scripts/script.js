96ae2e89179357232257539530127918a313069b
KevR98 <ramil.kevin98@gmail.com>
2025-07-10 19:21:29 +0200

    Fixed the code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,8 @@
-const urlHamster = 'https://api.pexels.com/v1/search?query=hamsters';
-const urlTigers = 'https://api.pexels.com/v1/search?query=tigers';
+const URL = 'https://api.pexels.com/v1/search?query=';
 const api = 'EWSPQiHinqL5vC1sForGvbNwTZklElsUkqVUtxokDCasRmMVndz1Do3q';
 
-const firstpic = function () {
-  fetch(urlHamster, {
+const changeImage = function (animals) {
+  fetch(URL + animals, {
     headers: {
       Authorization: api,
     },
@@ -12,20 +11,17 @@ const firstpic = function () {
       if (res.ok) {
         return res.json();
       } else {
-        throw new Error('Response.ok non ha restituito true');
+        throw new Error('Errore nel caricamento delle immagini');
       }
     })
+
     .then((newImg) => {
-      console.log(newImg);
-      const loadNewImg = document.getElementById('loadImage');
       const img = document.querySelectorAll('.card-img-top');
-      loadNewImg.addEventListener('click', () => {
-        for (let i = 0; i < img.length; i++) {
-          img[i].setAttribute('src', newImg.photos[i].src.small);
-        }
-      });
+      for (let i = 0; i < img.length; i++) {
+        img[i].setAttribute('src', newImg.photos[i].src.small);
+      }
       const hide = document.querySelectorAll('.btn-group .btn:nth-of-type(2)');
-      hide.forEach((btn, i) => {
+      hide.forEach((btn) => {
         btn.innerHTML = 'Hide';
         btn.addEventListener('click', function () {
           btn.closest('.col-md-4').classList.add('d-none');
@@ -36,38 +32,21 @@ const firstpic = function () {
         newId.innerHTML = `${newImg.photos[i].id}`;
       });
     })
+
     .catch((err) => {
-      console.log('ERRORE', err);
+      console.log('Errore', err);
     });
 };
 
-const secondpic = function () {
-  fetch(urlTigers, {
-    headers: {
-      Authorization: api,
-    },
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error('Response.ok non ha restituito true');
-      }
-    })
-    .then((newImg) => {
-      console.log(newImg);
-      const loadNewImg = document.getElementById('loadSecondaryImage');
-      const img = document.querySelectorAll('.card-img-top');
-      loadNewImg.addEventListener('click', () => {
-        for (let i = 0; i < img.length; i++) {
-          img[i].setAttribute('src', newImg.photos[i].src.small);
-        }
-      });
-    })
-    .catch((err) => {
-      console.log('ERRORE', err);
-    });
+const imgBtn = function () {
+  const firstBtn = document.getElementById('loadImage');
+  firstBtn.addEventListener('click', () => {
+    changeImage('hamsters');
+  });
+  const secondBtn = document.getElementById('loadSecondaryImage');
+  secondBtn.addEventListener('click', () => {
+    changeImage('tigers');
+  });
 };
 
-firstpic();
-secondpic();
+imgBtn();
